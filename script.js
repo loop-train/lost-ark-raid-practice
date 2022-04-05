d6c92e5f9e19bc5371df3bf8fdbccdc6fae08a46
loop-train <loop.train.dev@gmail.com>
2022-04-05 23:23:51 +0900

    use window.getComputedStyle

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ player.style.marginLeft = playground.clientWidth / 2;
 player.style.marginTop = playground.clientHeight / 2;
 
 playground.addEventListener('click', e => {
-    var currentX = parseInt(player.style.marginLeft.replace("px",""));
-    var currentY = parseInt(player.style.marginTop.replace("px", ""));
+    var currentX = parseInt(window.getComputedStyle(player).marginLeft.replace("px",""));
+    var currentY = parseInt(window.getComputedStyle(player).marginTop.replace("px", ""));
     var clickedX = e.clientX;
     var clickedY = e.clientY;
 
-    var diffX = clickedX - currentX;
-    var diffY = clickedY - currentY;
+    var diffX = currentX - clickedX;
+    var diffY = currentY - clickedY;
 
     var marginLeft = calcMarginLeft(clickedX, currentX, diffX);
     var marginTop = calcMarginTop(clickedY, currentY, diffY);
 
-    var distance = Math.round(Math.sqrt(diffX*diffX, diffY*diffY), 2);
-    const duration = Math.abs(distance + 1) * 10;
+    var distance = Math.round(Math.sqrt(diffX*diffX + diffY*diffY), 2);
+    const duration = Math.abs(distance) * 15;
+    console.log("distance", distance);
+    console.log("duration", duration);
 
     player.animate(
     {
@@ -25,7 +27,7 @@ playground.addEventListener('click', e => {
         marginTop: marginTop + "px"
     }, 
     {
-        duration: 2000,
+        duration: duration,
         fill: "forwards"
     });
 });
@@ -34,16 +36,14 @@ function calcMarginLeft(clickedX, currentX, diffX) {
     if (false) {
         return clickedX - player.clientWidth / 2;
     } else {
-        return Math.min(currentX - diffX, playground.clientWidth);
+        return Math.min(Math.max(currentX + diffX - player.clientWidth / 2, 0), playground.clientWidth);
     }
-    // return newX - player.clientWidth
 }
 
 function calcMarginTop(clickedY, currentY, diffY) {
     if (false) {
         return clickedY - player.clientHeight / 2;
     } else {
-        return Math.min(currentY - diffY, playground.clientHeight);
+        return Math.min(Math.max(currentY + diffY - player.clientHeight / 2, 0), playground.clientHeight);
     }
-    // return newY - title.offsetHeight - player.clientHeight;
 }
